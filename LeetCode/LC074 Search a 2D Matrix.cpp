012e2721c03b9c2cdba8c84fc59a1861e72a7bd7
Carlisle Cheng <carlislecheng@gmail.com>
2019-07-03 21:21:29 +0800

    Update LC074 Search a 2D Matrix.cpp

--- LeetCode/LC074 Search a 2D Matrix.cpp
+++ LeetCode/LC074 Search a 2D Matrix.cpp
@@ -14,33 +14,37 @@ class Solution
 public:
 	bool searchMatrix(vector<vector<int>>& matrix, int target)
 	{
+		if (matrix.empty() || matrix[0].empty())
+			return false;
+		if (target < matrix[0][0] || target > *(matrix.rbegin()->rbegin()))
+			return false;
+
 		int left = 0;
-		int right = matrix.size();
+		int right = matrix.size() - 1;
 		int mid = (left + right) / 2;
-		while (matrix[mid][0] > target || target >= matrix[mid+1][0])
+		while (left <= right)
 		{
 			mid = (left + right) / 2;
 			if (matrix[mid][0] > target)
-				right = mid;
+				right = mid - 1;
 			else
-				left = mid;
+				left = mid + 1;
 		}
 
-		int row = mid;
+		int row = left - 1;
 		left = 0;
-		right = matrix[mid].size();
+		right = matrix[row].size() - 1;
 		mid = (left + right) / 2;
-		while (left < right)
+		while (left <= right)
 		{
+			mid = (left + right) / 2;
 			if (matrix[row][mid] == target)
 				return true;
-			if (matrix[row][mid] < target && target < matrix[row][mid + 1])
-				return false;
 
 			if (matrix[row][mid] > target)
-				right = mid;
+				right = mid - 1;
 			else
-				left = mid;
+				left = mid + 1;
 		}
 
 		return false;
