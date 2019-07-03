944bc27f73c23e05591ce47b094f2a6cba8eda81
Carlisle Cheng <carlislecheng@gmail.com>
2019-07-03 20:07:50 +0800

    Update LC089 Gray Code.cpp

--- LeetCode/LC089 Gray Code.cpp
+++ LeetCode/LC089 Gray Code.cpp
@@ -14,47 +14,12 @@ class Solution
 public:
 	vector<int> grayCode(int n)
 	{
-		int len = pow(2, n);
-		vector<int> ans(len, 0);
-		vector<vector<int>> bin{ {0}, {1} };
+		int m = 1 << n;
+		vector<int> ans;
 
-		for (int i = 1; i < n; ++i)
-		{
-			int m = pow(2, i);
-			for (int j = m - 1; j >= 0; --j)
-			{
-				auto temp = bin[j];
-				bin.push_back(temp);
-			}
-
-			int j;
-			for (j = 0; j < m; ++j)
-			{
-				bin[j].push_back(0);
-			}
-			for (; j < 2 * m; ++j)
-			{
-				bin[j].push_back(1);
-			}
-		}
-		
-		for (int i = 0; i < len; ++i)
-		{
-			for (int j = 0; j < n; ++j)
-			{
-				ans[i] += pow(2, j) * bin[i][j];
-			}
-		}
+		for (int i = 0; i < m; i++)
+			ans.push_back(i ^ i >> 1);
 
 		return ans;
 	}
 };
-
-int main()
-{
-	int n = 2;
-	Solution res;
-	res.grayCode(n);
-
-	return 0;
-}
