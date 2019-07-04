9340b7b69bb88eb74add8ee5e65aec5e45196df9
Carlisle Cheng <carlislecheng@gmail.com>
2019-07-04 16:54:25 +0800

    Update LC124 Binary Tree Maximum Path Sum.cpp

--- LeetCode/LC124 Binary Tree Maximum Path Sum.cpp
+++ LeetCode/LC124 Binary Tree Maximum Path Sum.cpp
@@ -14,41 +14,28 @@ class Solution
 public:
 	int maxPathSum(TreeNode* root)
 	{
-		if (!root)
-			return 0;
-
 		int ans = INT_MIN;
 		maxSum(root, ans);
 
 		return ans;
 	}
 
-	pair<int, int> maxSum(TreeNode* root, int& ans)
+	int maxSum(TreeNode* root, int& ans)
 	{
-		int mid = root->val;
-		pair<int, int> left, right;
-		left = make_pair(INT_MIN, INT_MIN);
-		right = left;
+		int left = 0, right = 0;
 
 		if (root->left)
-		{
-			left = maxSum(root->left, ans);
-			mid += left.second;
-		}
+			left = max(0, maxSum(root->left, ans));
 		if (root->right)
-		{
-			right = maxSum(root->right, ans);
-			mid += right.second;
-		}
+			right = max(0, maxSum(root->right, ans));
 
-		int tolMax = max(mid, max(left.first, right.first));
-		int midMax = root->val + max(left.second, right.second);
+		ans = max(ans, root->val + left + right);
 
-		if (!root->left && !root->right)
-			midMax = root->val;
+		return root->val + max(left, right);
+	}
 
-		ans = max(ans, tolMax);
-
-		return pair<int, int>(tolMax, midMax);
+	inline int max(int a, int b)
+	{
+		return a > b ? a : b;
 	}
 };
