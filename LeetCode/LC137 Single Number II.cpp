a99841290908a2755ddc0a92521d07ffb73a7ae9
Carlisle Cheng <carlislecheng@gmail.com>
2019-07-04 15:16:19 +0800

    Update LC137 Single Number II.cpp

--- LeetCode/LC137 Single Number II.cpp
+++ LeetCode/LC137 Single Number II.cpp
@@ -14,7 +14,15 @@ class Solution
 public:
 	int singleNumber(vector<int>& nums)
 	{
-		int ones, twos, threes;
+		int ones = 0, twos = 0;
+
+		for (int num : nums)
+		{
+			ones = (ones ^ num) & ~twos;
+			twos = (twos ^ num) & ~ones;
+		}
+
+		return ones;
 	}
 };
 
