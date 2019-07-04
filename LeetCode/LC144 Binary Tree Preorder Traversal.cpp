f74d82a24ccbcd3a3f35ccaec3bd96602745dd3d
Carlisle Cheng <carlislecheng@gmail.com>
2019-07-04 15:32:41 +0800

    Update LC144 Binary Tree Preorder Traversal.cpp

--- LeetCode/LC144 Binary Tree Preorder Traversal.cpp
+++ LeetCode/LC144 Binary Tree Preorder Traversal.cpp
@@ -9,3 +9,40 @@ static const auto ioSyncOff = []()
 	return 0;
 }();
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+	vector<int> preorderTraversal(TreeNode* root)
+	{
+		if (!root)
+			return {};
+
+		stack<TreeNode*> st;
+		st.push(root);
+		
+		vector<int> ans;
+		auto cur = root;
+		while (!st.empty())
+		{
+			cur = st.top();
+			st.pop();
+			ans.push_back(cur->val);
+
+			if (cur->right)
+				st.push(cur->right);
+			if (cur->left)
+				st.push(cur->left);
+		}
+
+		return ans;
+	}
+};
