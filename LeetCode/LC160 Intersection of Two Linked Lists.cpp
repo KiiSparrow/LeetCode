40c6a0ddcd6b99186a156ebd09f513f41657ce95
Carlisle Cheng <carlislecheng@gmail.com>
2019-07-08 09:36:54 +0800

    Create LC160 Intersection of Two Linked Lists.cpp

---
+++ LeetCode/LC160 Intersection of Two Linked Lists.cpp
@@ -0,0 +1,36 @@
+#include "LeetCode.h"
+using namespace std;
+
+
+static const auto ioSyncOff = []()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+	{
+		ListNode* pa = headA;
+		ListNode* pb = headB;
+
+		while (pa != pb)
+		{
+			pa = (pa->next ? pa->next : headB);
+			pb = (pb->next ? pb->next : headA);
+		}
+
+		return pa;
+	}
+};
