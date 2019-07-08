ae8032efeff85c67307de2e19c42a5d27acba52f
Carlisle Cheng <carlislecheng@gmail.com>
2019-07-08 08:57:36 +0800

    Create LC206 Reverse Linked List.cpp

---
+++ LeetCode/LC206 Reverse Linked List.cpp
@@ -0,0 +1,27 @@
+#include "LeetCode.h"
+using namespace std;
+
+
+static const auto ioSyncOff = []()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+	ListNode* reverseList(ListNode* head)
+	{
+
+	}
+};
