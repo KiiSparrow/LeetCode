982ddf7de72c41f8bec26c37d9bd4e5a51088c71
Carlisle Cheng <carlislecheng@gmail.com>
2019-07-08 09:51:08 +0800

    Update LC169 Majority Element.cpp

--- LeetCode/LC169 Majority Element.cpp
+++ LeetCode/LC169 Majority Element.cpp
@@ -14,6 +14,31 @@ class Solution
 public:
 	int majorityElement(vector<int>& nums)
 	{
+		int ans;
+		int counter = 0;
+
+		for (int n : nums)
+		{
+			if (!counter)
+			{
+				ans = n;
+				counter = 1;
+			}
+			else if (ans == n)
+			{
+				++counter;
+			}
+			else
+			{
+				--counter;
+			}
+		}
+
+		return ans;
+	}
+
+	int majorityElement_map(vector<int>& nums)
+	{
 		int ans = 0;
 		int max = 0;
 		unordered_map<int, int> nmap;
