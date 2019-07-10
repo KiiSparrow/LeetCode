e5fe03e2cfc951d3f36ebbb180a511a72932a08a
Carlisle Cheng <carlislecheng@gmail.com>
2019-07-10 15:20:21 +0800

    Create LC461 Hamming Distance.cpp

---
+++ LeetCode/LC461 Hamming Distance.cpp
@@ -0,0 +1,36 @@
+#include "LeetCode.h"
+using namespace std;
+
+
+static const auto ioSyncOff = []()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
+
+class Solution
+{
+public:
+	static int hammingDistance(int x, int y)
+	{
+		int ans = 0;
+		int ham = x ^ y;
+
+		while (ham)
+		{
+			if (ham & 1)
+				++ans;
+			ham >>= 1;
+		}
+
+		return ans;
+	}
+};
+
+int main()
+{
+	Solution::hammingDistance(1, 4);
+
+	return 0;
+}
