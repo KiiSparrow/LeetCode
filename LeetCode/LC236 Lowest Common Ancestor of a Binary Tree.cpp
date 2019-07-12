84cd50238f4b7728caadec70c78f41a176e347d8
Carlisle Cheng <carlislecheng@gmail.com>
2019-07-12 15:50:18 +0800

    Create LC236 Lowest Common Ancestor of a Binary Tree.cpp

---
+++ LeetCode/LC236 Lowest Common Ancestor of a Binary Tree.cpp
@@ -0,0 +1,28 @@
+#include "LeetCode.h"
+using namespace std;
+
+
+static const auto ioSyncOff = []()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+	{
+
+	}
+};
