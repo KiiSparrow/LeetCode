816566d7a58391fb1a78d4b283b6f0efa3701414
Carlisle Cheng <carlislecheng@gmail.com>
2019-07-15 21:54:02 +0800

    2019-7-15 21:54:00

--- LeetCode/LC375 Guess Number Higher or Lower II.cpp
+++ LeetCode/LC375 Guess Number Higher or Lower II.cpp
@@ -14,17 +14,26 @@ class Solution
 public:
 	int getMoneyAmount(int n)
 	{
-		int left = 1;
-		int right = n;
-		int ans = 0;
-
-		while (left < right)
+		vector<vector<int>> dp(n + 1, vector<int>(n + 1));
+		for (int i = n; i > 0; --i)
 		{
-			int mid = (left + right) / 2;
-			ans += mid;
-			left = mid + 1;
+			for (int j = i + 1; j <= n; ++j)
+			{
+				if (i == j)
+					dp[i][j] = 0;
+				else if (i + 1 == j)
+					dp[i][j] = i;
+				else
+				{
+					dp[i][j] = INT_MAX;
+					for (int p = i + 1; p < j; ++p)
+					{
+						dp[i][j] = min(dp[i][j], p + max(dp[i][p - 1], dp[p + 1][j]));
+					}
+				}
+			}
 		}
 
-		return ans;
+		return dp[1][n];
 	}
 };
